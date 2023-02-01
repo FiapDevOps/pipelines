5a60858ae82b7a309c164649aa2adb31f8697351
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-01-31 22:10:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to FIAP</h1>
+          <h1 className="App-title">Welcome to FIAP 43SCJ</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
