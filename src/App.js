376054c61b5335cd03fde7521e7ed00e2a2e2a51
lamartinesyshard <125418508+lamartinesyshard@users.noreply.github.com>
2023-02-14 22:11:42 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to FIAP 43SCJ</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Roberto Lamartine
         </p>
       </div>
     );
