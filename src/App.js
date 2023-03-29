9bf1224a38363c3bcffca5d6d87611ad149ed16c
victorf16 <victorf_16@hotmail.com>
2023-03-29 20:18:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          Made by: Gabriel, Marina and Victor
+          Made by: Gabriel, Marina and Victor, Teste Victor
 
         </p>
         <img src={team} alt="team" />
